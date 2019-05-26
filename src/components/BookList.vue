ef90d224d2594305ad392837be8129606fa80066
Alisha Patel <C54097>
2019-05-26 19:10:33 +0200

    form changes

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,15 +2,19 @@
     <div>
         <h1>{{title}}</h1>
         <ul > 
-            <book-item v-for="book in books"  :book='book'></book-item>
+            <book-item v-for="book in books" :book='book'></book-item>
         </ul>
+        <hr>
+        <book-form @addBook='appendBook'></book-form>
     </div>
 </template>
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 export default {
     components : {
-        BookItem
+        BookItem,
+        BookForm
     },
      name:"BookList",
      data() {
@@ -21,6 +25,11 @@ export default {
                 {title: 'American Gods', author: 'Neil Gaiman'},
                 {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
             ]
+         }
+     },
+     methods : {
+         appendBook(bookTitle, bookAuthor){
+             this.books.push({title: bookTitle, author: bookAuthor});
          }
      }
     
