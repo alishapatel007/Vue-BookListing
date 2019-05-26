fbf8316baa11411c5821bce18c07155dee3d1ea1
Alisha Patel <C54097>
2019-05-26 18:24:00 +0200

    child component

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>{{title}}</h1>
         <ul > 
-            <li v-for="book in books">{{book.title}} : {{book.author}}</li>
+            <li v-for="book in books"></li>
         </ul>
     </div>
 </template>
@@ -31,12 +31,6 @@ export default {
         list-style-type: none;
         padding: 0;
     }
-    li{
-        display: block;
-        margin: 0 10px;
-
-    }
-
 </style>
 
 
